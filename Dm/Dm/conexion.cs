59ff0569971fa5f9b533f97653321832b7260037
DmGil <36966908+DmGil@users.noreply.github.com>
2018-03-01 12:29:29 -0600

    Adicionar codigo de proyecto Dm

---
+++ Dm/Dm/conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+namespace Dm
+{
+    class conexion
+    {
+        SqlConnection cm;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public conexion()
+        {
+            try
+            {
+                cn = new SqlConnection(Data Source = 192.168.100.250; Initial Catalog = Inventario; Persist Security Info = True; User ID = sa; Password = ***********; MultipleActiveResultSets = True)
+                cn.open();
+                MessageBox.Show("exito");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("no hay" + ex.ToString();
+            }
+
+        }
+    }
+}
